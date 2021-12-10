5533275fd2069dd3d27c6616641084a4b3615905
Pulo <paulo2001.yt@gmail.com>
2021-12-10 11:47:46 -0300

    Contador aula remota

---
+++ contador.js
@@ -0,0 +1,30 @@
+let numeroDeAlunos = 10
+let contador = 0
+
+while(contador <= numeroDeAlunos){
+
+if (contador == 0) {
+    console.log("O número atual é zero")
+        
+    }else if (contador % 2 == 0){
+        console.log("O número " + contador + " é par")
+    }
+    else{
+        console.log(`O número ${contador} é impar`)
+    }
+    contador ++
+}
+
+// for (let contador = 0; contador <= numeroDeAlunos.length; contador++) {
+//     const element = numeroDeAlunos[contador];
+    
+// }
+// if (contador == 0) {
+//     console.log("O número atual é zero")
+    
+// }else if (contador % 2 == 0){
+//     console.log("O número " + contador + " é par")
+// }
+// else{
+//     console.log(`O número ${contador} é impar`)
+// }
