02227a7b2161938f9ba644927fb6ced62af12677
Pulo <paulo2001.yt@gmail.com>
2021-12-10 11:55:34 -0300

    Contador

--- contador.js
+++ contador.js
@@ -1,19 +1,19 @@
-let numeroDeAlunos = 10
-let contador = 0
+// let numeroDeAlunos = 10
+// let contador = 0
 
-while(contador <= numeroDeAlunos){
+// while(contador <= numeroDeAlunos){
 
-if (contador == 0) {
-    console.log("O número atual é zero")
+// if (contador == 0) {
+//     console.log("O número atual é zero")
         
-    }else if (contador % 2 == 0){
-        console.log("O número " + contador + " é par")
-    }
-    else{
-        console.log(`O número ${contador} é impar`)
-    }
-    contador ++
-}
+//     }else if (contador % 2 == 0){
+//         console.log("O número " + contador + " é par")
+//     }
+//     else{
+//         console.log(`O número ${contador} é impar`)
+//     }
+//     contador ++
+// }
 
 // for (let contador = 0; contador <= numeroDeAlunos.length; contador++) {
 //     const element = numeroDeAlunos[contador];
@@ -28,3 +28,10 @@ if (contador == 0) {
 // else{
 //     console.log(`O número ${contador} é impar`)
 // }
+
+let quantidadeAlunos = 10
+let listaAlunos = ["Marcel", "Franklin", "Rita", "Afrânio"]
+
+for (let aluno of listaAlunos) {
+    console.log(`Este aluno se chama ${aluno}`)
+}
